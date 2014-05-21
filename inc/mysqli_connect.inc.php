76ff40016e21fa0f405980f8242c199b47313d2b
Troy Uyan <TroyUyan@users.noreply.github.com>
2014-05-20 17:21:10 -0700

    Delete mysqli_connect.inc.php

--- inc/mysqli_connect.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$dbc=mysqli_connect("127.0.0.1","gwb_admin","gwblogin1","gwb_training")
-	OR die("<p>Could not connect to the MySQL Server: " . mysqli_connect_error() . "<p>");
-	
-	//set the endcoding
-	mysqli_set_charset($dbc,'utf8');
-?>
