2ee86def410e68d3b8b88a16e296f64bbca9023b
savrawr <s.lord@students.clark.edu>
2014-05-22 17:54:54 -0700

    Create nav.inc.php

---
+++ inc/nav.inc.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<div class="nav">
+				<ul>
+					<li id="home"><a href="index.php" alt="Return to Greenwell Bank Home Page">HOME</a></li>
+					<li id="user"><a href="user.php" alt="View the users list">USERS</a></li>
+
+					<li id="courses"><a href="courses.php" alt="View courses list">COURSES</a></li>
+					<li id="departments"><a href="departments.php" alt="Logout">DEPARTMENTS</a></li>
+					<li id="logout"><a href="logout.php" alt="Logout">LOG OUT</a></li>
+				</ul>
+			</div> <!-- end nav -->
+</body>
+</html>
