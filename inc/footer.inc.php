decdd1cf224001d5eb095fc39cfbe5f208d6f2b0
Troy Uyan <blutank24@msn.com>
2014-05-27 16:10:44 -0700

    full-out

--- inc/footer.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include('footer_html.inc.php');
-	
-	//close database connection	
-	#mysql_close($dbc);
-?>
--- inc/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function logincheck() {
-		if ($_SESSION['loggedin'] !== 1) {
-			// header('Location: http://localhost/training-management-system/index.php') ;
-			// NEEDS TO BE ON LIVE SERVER -- NEED URL
-		}
-	}
-
-?>
