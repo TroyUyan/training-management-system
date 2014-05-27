3ee7ef4f80c3d90f8c6cb2dd7fb7a87e4b3fdb91
Troy Uyan <blutank24@msn.com>
2014-05-27 16:35:58 -0700

    main.php changes yo

--- main.php
+++ main.php
@@ -7,48 +7,60 @@
 	<h3>Welcome <?php echo "{$_SESSION['first_name']} {$_SESSION['last_name']}";?>!</h3>
 
 	<?php
-		if ($_SESSION['usergroup_id'] == 1) {
-
-			#Employee
-			echo "Employee";
-
+		if ($_SESSION['usergroup_id'] == 4) {
 			
-		} elseif ($_SESSION['usergroup_id'] == 2) {
-
-			#Coordinator
-			echo "Coordinator";
-
-
-		} elseif ($_SESSION['usergroup_id'] == 3) {
-
-			#Viewer
-			echo "Viewer";
-
-
-		} elseif ($_SESSION['usergroup_id'] == 4) {
-
 			#Admin
 			echo "<p>Usergroup: Admin</p>";
 			?>
+
+			<div class="tablesection">
+				<h3>Users Table</h3>
+				<a href="users_add.php"><p class="button">Add New User</p></a>
+				<a href="#"><p class="button">View All Users</p>
+				<a href="#"><p class="button">Edit Employee Records</p>
+			</div>
 
 			<table>
 				<tr>
 					<th>Users Table</th>
 				</tr>
 				<tr>
-					<td><a href="users_add.php">Add New User</td>
+					<td class="button"><a href="users_add.php">Add New User</td>
 				</tr>
 				<tr>
-					<td>View All Users</td>
+					<td class="button"><a href="#">View All Users</a></td>
 				</tr>
 				<tr>
-					<td>Edit Employee Records</td>
+					<td class="button"><a href="#">Edit Employee Records</a></td>
 				</tr>
 			</table>
 
 		<?php
+
+			
+		} elseif ($_SESSION['usergroup_id'] == 3) {
+
+			#Viewer
+			echo "Viewer";
+
+
+
+		} elseif ($_SESSION['usergroup_id'] == 2) {
+
+			#Coordinator
+			echo "Coordinator";
+
+
+		} elseif ($_SESSION['usergroup_id'] == 1) {
+
+			#Employee
+			echo "Employee";
+
+			
 		} else {
+
 			#error-out
+
 		}
 
 	?>
