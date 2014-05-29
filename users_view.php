44ab568035199051d1edfcad2efac67b35402dac
Troy Uyan <blutank24@msn.com>
2014-05-29 13:25:58 -0700

    users_view admin icons

--- users_view.php
+++ users_view.php
@@ -18,25 +18,25 @@
 
             echo "<table>\n";
             echo '<tr>
-            		<th>Actions</th>
             		<th><a href="?sort=#">User ID</a></th>
             		<th><a href="?sort=#">Username</a></th>
             		<th><a href="?sort=#">First Name</a></th>
             		<th><a href="?sort=#">Last Name</a></th>
-            		<th><a href="?sort=#">Usergroup ID</a></th>
-            		<th><a href="?sort=#">Department ID</a></th>
+            		<th><a href="?sort=#">Group ID</a></th>
+            		<th><a href="?sort=#">Dept. ID</a></th>
+                    <th>Actions</th>
             	  </tr>' . "\n";
-            # loop thru each record in the students table
+            # loop thru each record in the users table
             while ($row = mysqli_fetch_array($result)){
                     echo "<tr class=\"center\">\n";
                     echo "
-                    	<td><a href=\"?edit={$row['user_id']}\">Edit</a> <a href=\"?delete={$row['user_id']}\" onclick=\"return confirm('Are you sure?');\">Delete</a></td>
                     	<td>{$row['user_id']}</td>
                     	<td>{$row['username']}</td>
                     	<td>{$row['first_name']}</td>
                     	<td>{$row['last_name']}</td>
                     	<td>{$row['usergroup_id']}</td>
-                    	<td>{$row['department_id']}</td>\n";
+                    	<td>{$row['department_id']}</td>
+                        <td><a href=\"?edit={$row['user_id']}\"><img src='img/ico_edit.png' alt='Edit' title='Edit'></a> <a href=\"?delete={$row['user_id']}\" onclick=\"return confirm('Are you sure?');\"><img src='img/ico_x.png' alt='Delete' title='Delete'></a></td>\n";
                     echo "</tr>\n";
             } # end of while loop
             echo "</table>\n";
