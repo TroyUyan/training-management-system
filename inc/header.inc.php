676277364eeb56d33e661630cbf530d1959a5523
44watdesign <christi6886@gmail.com>
2014-05-31 11:18:40 -0700

    Please test nav

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,6 +1,7 @@
 <?php
 	session_start();
 	include('header_html.inc.php');
+	include('nav.inc.php');
 	require('mysqli_connect.inc.php');
 	include('functions.inc.php');
 ?>
--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,19 +1,67 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title></title>
-</head>
-<body>
-	<div class="nav">
-				<ul>
-					<li id="home"><a href="index.php" alt="Return to Greenwell Bank Home Page">HOME</a></li>
-					<li id="user"><a href="user.php" alt="View the users list">USERS</a></li>
+			<div class="nav">
 
-					<li id="courses"><a href="courses.php" alt="View courses list">COURSES</a></li>
-					<li id="departments"><a href="departments.php" alt="Logout">DEPARTMENTS</a></li>
-					<li id="logout"><a href="logout.php" alt="Logout">LOG OUT</a></li>
-				</ul>
+		    <?php
+		    //create navigation for login page
+		    if (!isset($_SESSION)) {
+		    ?>
+		        <ul>
+		        	<li id="home"><a href=#wrapper alt="Return to Greenwell Bank Home">Home</a><li>
+		        </ul>
+		    <?php
+		        //navigation for admin users
+		        } elseif ($_SESSION['usergroup_id'] == 4) {
+		    ?>
+						<ul>
+							<li id="home"><a href="main.php" alt="Admin Home Page">HOME</a></li>
+							<li id="employee"><a href="employee_records.php" alt="Edit Employee Records">EMPLOYEE RECORDS</a></li>
+							<li id="courses"><a href="courses.php" alt="Edit Courses">COURSES</a></li>
+							<li id="departments"><a href="departments.php" alt="Edit Departments">DEPARTMENTS</a></li>
+							<li id="user"><a href="users_view.php" alt="Edit Users">USERS</a></li>
+							<li id="logout"><a href="logout.php" alt="Logout">LOG OUT</a></li>
+						</ul>
+		    <?php
+		        //navigation for viewer users
+		        } elseif ($_SESSION['usergroup_id'] == 3){
+		    ?>
+				    <ul>
+				    	<li id="home"><a href="main.php" alt="Viewer Home Page">HOME</a></li>
+						<li id="employee"><a href="employee_records.php" alt="View Employee Records">EMPLOYEE RECORDS</a></li>
+						<li id="courses"><a href="courses.php" alt="View Courses">COURSES</a></li>
+						<li id="departments"><a href="departments.php" alt="View Departments">DEPARTMENTS</a></li>
+						<li id="user"><!-- <a href="users_view.php" alt="View Users">USERS</a> --></li>
+						<li id="logout"><a href="logout.php" alt="Logout">LOG OUT</a></li>	
+					</ul>
+		    <?php
+		        //navigation for coordinator users
+		        } elseif ($_SESSION['usergroup_id'] == 2){
+		    ?>
+		             <ul>
+		        		<li id="home"><a href="main.php" alt="Coordinator Home Page">HOME</a></li>
+						<li id="employee"><a href="employee_records.php" alt="Edit Employee Records">EMPLOYEE RECORDS</a></li>
+						<li id="courses"><a href="courses.php" alt="Add Courses">COURSES</a></li>
+						<!-- view only for departments -->
+						<li id="departments"><a href="departments.php" alt="View Departments">DEPARTMENTS</a></li>
+						<li id="user"><!-- <a href="users_view.php" alt="View Users">USERS</a> --></li>
+						<li id="logout"><a href="logout.php" alt="Logout">LOG OUT</a></li>	
+		             </ul>
+		    <?php
+		        //navigation for employee users and default for fail
+		        } else {
+		    ?>
+	                <ul>
+	            		<li id="home"><!-- <a href="main.php" alt="Admin Home Page">HOME</a> --></li>
+						<li id="employee"><!-- <a href="employee_records.php" alt="View Employee Records">EMPLOYEE RECORDS</a> --></li>
+						<li id="courses"><!-- <a href="courses.php" alt="View Courses">COURSES</a> --></li>
+						<li id="departments"><!-- <a href="departments.php" alt="View Departments">DEPARTMENTS</a> --></li>
+						<li id="user"><!--<a href="users_view.php" alt="View Users">USERS</a> --></li>
+						<li id="logout"><a href="logout.php" alt="Logout">LOG OUT</a></li>					
+	                </ul>
+	        <?php
+	            } //close nav if
+	        ?>
+                
 			</div> <!-- end nav -->
-</body>
-</html>
+		</header>
+
+		<div id="content" >
+
