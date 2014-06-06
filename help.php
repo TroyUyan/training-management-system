d758114662003b3350bf7fc6a205dc7744e8409a
savrawr <s.lord@students.clark.edu>
2014-06-05 17:39:09 -0700

    added style

--- help.php
+++ help.php
@@ -3,11 +3,13 @@
 	include ('inc/header.inc.php');
 ?>
 
+<div id="content">
 <h1>Need Help?</h1>
-<p>If you're having trouble logging in, you may have typed your username/password wrong. Another reason may be your administrator has not set up your acccount yet. </p>
+<p>If you're having trouble logging in, you may have typed your username/password wrong. </p>
+<p>Another reason may be your administrator has not set up your acccount yet. </p>
 
 
-
+</div>
 
 <?php 
 	include('inc/footer.inc.php');
