256c28acf3df3da31bb87b4ff8eaf3addf3c74a0
savrawr <s.lord@students.clark.edu>
2014-06-05 17:40:57 -0700

    Update help.php

--- help.php
+++ help.php
@@ -3,7 +3,7 @@
 	include ('inc/header.inc.php');
 ?>
 
-<div id="content">
+<div id="content1">
 <h1>Need Help?</h1>
 <p>If you're having trouble logging in, you may have typed your username/password wrong. </p>
 <p>Another reason may be your administrator has not set up your acccount yet. </p>
