4501d58c61d4ffadfbd6c36dcae8b36b47b43599
savrawr <s.lord@students.clark.edu>
2014-06-05 17:09:21 -0700

    see your

---
+++ help.php
@@ -0,0 +1,14 @@
+<?php
+	$page_title=" | Help";
+	include ('inc/header.inc.php');
+?>
+
+<h1>Need Help?</h1>
+<p>If you're having trouble logging in, you may have typed your username/password wrong. Another reason may be your administrator has not set up your acccount yet. </p>
+
+
+
+
+<?php 
+	include('inc/footer.inc.php');
+?>
