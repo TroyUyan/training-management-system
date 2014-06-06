834fa325ca097514bba36d7f8f77b9fba0b05760
Troy Uyan <blutank24@msn.com>
2014-06-06 12:41:58 -0700

    courses_add complete

--- courses_add.php
+++ courses_add.php
@@ -13,66 +13,60 @@
 
 			if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-				if(!empty($_POST['department_id']) &&
-					!empty($_POST['department_name']) &&
-					!empty($_POST['required_courses']) &&
-					!empty($_POST['department_desc'])
+				if(!empty($_POST['course_name']) &&
+					!empty($_POST['department_id'])
 					) {
 
+						$course_name = $_POST['course_name'];
 						$department_id = $_POST['department_id'];
-						$department_name = $_POST['department_name'];
-						$required_courses = $_POST['required_courses'];
-						$department_desc = $_POST['department_desc'];
-
-						# check if id exists
-	          $sql = "SELECT department_id FROM departments WHERE department_id = $department_id";
-	          $result = mysqli_query($dbc, $sql);
-
-	          if (mysqli_num_rows($result) == 0) {
 	            
-	            $sql="INSERT INTO gwb_training.departments (department_id, department_name, department_desc, required_courses)
-										VALUES ('$department_id', '$department_name', '$department_desc', '$required_courses')";
+            $sql="INSERT INTO gwb_training.courses (course_id, course_name, department_id)
+									VALUES (NULL, '$course_name', '$department_id')";
 
-	            mysqli_query($dbc, $sql);
+            mysqli_query($dbc, $sql);
 
-	            if (mysqli_affected_rows($dbc) == 1) {
-	                echo "<p><img src='img/ico_true.png'> The department was successfully added to the database!</p>";
-									echo "<p>You can add another below, or <a href='departments_view.php'>view the departments table</a>.</p>";
-	            } else {
-	                echo "<p><img src='img/ico_false.png'> Something has gone wrong, here is the SQL:<br>$sql</p>";
-	            }
-
-	          } else {
-	            echo "<p><img src='img/ico_false.png'> That department ID is already in use! Please pick another.</p>";
-	          }
+            if (mysqli_affected_rows($dbc) == 1) {
+                echo "<p><img src='img/ico_true.png'> The course was successfully added to the database!</p>";
+								echo "<p>You can add another below, or <a href='courses_view.php'>view the courses table</a>.</p>";
+            } else {
+                echo "<p><img src='img/ico_false.png'> Something has gone wrong, here is the SQL:<br>$sql</p>";
+            }
 
 
 				} else {
-					echo "<p><img src='img/ico_false.png'> You did not fill out all of the form fields!</p>";
+					echo "<p><img src='img/ico_false.png'> You did not fill out all of the form fields! Make sure you pick a department.</p>";
 				} #End if with the fill-check
 				
 			}
 		
 		?>
 
-			<form method="POST" action="departments_add.php" class="inputform departmentform">
+			<form method="POST" action="courses_add.php" class="inputform departmentform">
 				<p>
-					<label>Department ID</label>
-					<input type="number" name="department_id" value="<?php if(isset($_POST['department_id'])){echo $_POST['department_id'];}?>">
+					<label>Course ID</label>
+					<input type="text" name="course_id" placeholder="AUTO" disabled="1">
 				</p>
 				<p>
-					<label>Department Name</label>
-					<input type="text" name="department_name" value="<?php if(isset($_POST['department_name'])){echo $_POST['department_name'];}?>">
+					<label>Course Name</label>
+					<input type="text" name="course_name" value="<?php if(isset($_POST['course_name'])){echo $_POST['course_name'];}?>">
 				</p>
 				<p>
-					<label>Required Courses</label>
-					<input type="number" name="required_courses" value="<?php if(isset($_POST['required_courses'])){echo $_POST['required_courses'];}?>">
+					<label>Department</label>
+					<select name="department_id">
+						<?php #enter php again
+
+              # query for department list
+              $sql = "SELECT * FROM departments";
+              $result = mysqli_query($dbc, $sql);
+
+              # display options based on dept. table contents
+              while ($rows = mysqli_fetch_array($result)) {
+                echo "<option value='{$rows['department_id']}'>{$rows['department_name']}</option>\n";
+              }
+            // exit php ?>
+					</select>
 				</p>
-				<p>
-					<label>Department Description</label>
-					<textarea name="department_desc" rows="4" cols="50"><?php if(isset($_POST['department_desc'])){echo $_POST['department_desc'];}?></textarea>
-				</p>
-				<input type="submit" value="Add New Department" class="button">
+				<input type="submit" value="Add New Course" class="button">
 			</form>
 
 			
