49d691564e8c7f9bdc4e9083b1dab4a712a38e60
44watdesign <christi6886@gmail.com>
2014-06-08 13:44:03 -0700

    css fixes

--- 404.php
+++ 404.php
@@ -2,10 +2,10 @@
 	$page_title="404";
 	include ('inc/header.inc.php');
 ?>
-    <div id="404">
-    	<h1>fourofour</h1>
+    <div id="fourofour">
+    	<h1>404</h1>
     	<img src="img/cryingkitty.png" alt="grey kitty crying">
-    	<h2>We're sorry. The page you were looking for can not be found.</h2>
+    	<h2 class="center">We're sorry. The page you were looking for can not be found.</h2>
     </div>
 		
 
