3c14be8ea2b38c720a67ef878280d839c7be77f4
savrawr <s.lord@students.clark.edu>
2014-06-09 14:58:40 -0700

    added button, more text

--- help.php
+++ help.php
@@ -6,9 +6,13 @@
 <div id="content1">
 <h1>Need Help?</h1>
 <p>If you're having trouble logging in, you may have typed your username/password wrong. </p>
-<p>Another reason may be your administrator has not set up your acccount yet. </p>
+<p>Please note that passwords are case sensitive. If your username or password was not correct the admin may not have setup account.</p> 
+<p>Please check with your trainning coordinator, we apologize for any inconvenience.</p>
 
+</div>
 
+<div class="tablesection">
+	<a href="index.php"><p class="sectionbutton">Go Back</p></a>
 </div>
 
 <?php 
